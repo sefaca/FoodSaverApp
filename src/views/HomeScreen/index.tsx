e25d23bf0e68d84dc4716b2664eb55f13650e6d7
sefaca <sefaca24@gmail.com>
2024-07-18 19:52:11 +0200

    feat: home screen added

--- src/views/HomeScreen/index.tsx
+++ src/views/HomeScreen/index.tsx
@@ -1,8 +1,93 @@
-import {Container, Text} from './styles';
-import type {Props} from './types';
+import React, {useState, useRef} from 'react';
+import {Button, Text} from 'react-native';
+import {RNCamera} from 'react-native-camera';
+import TextRecognition from 'react-native-text-recognition';
+import {
+  ButtonCameraContainer,
+  Camera,
+  Container,
+  PendingView,
+  TextRecognized,
+} from './styles';
 
-export const HomeScreen: Props = () => (
-  <Container>
-    <Text>Hi!</Text>
-  </Container>
+// Componente para mostrar una vista de carga mientras la cámara se inicializa
+const PendingViewComponent = () => (
+  <PendingView>
+    <Text>Loading...</Text>
+  </PendingView>
 );
+
+export const HomeScreen = () => {
+  const [imageUri, setImageUri] = useState(null);
+  const [recognizedText, setRecognizedText] = useState('');
+  const cameraRef = useRef(null);
+
+  const takePicture = async camera => {
+    const options = {quality: 0.5, base64: true};
+    const data = await camera.takePictureAsync(options);
+    setImageUri(data.uri);
+
+    // Reconocimiento de texto en la imagen capturada
+    const textBlocks = await TextRecognition.recognize(data.uri);
+    console.log('Text Blocks:', textBlocks);
+
+    // Buscar la primera fecha con el formato xx/xx/xxxx, xx/xx, xx/xxxx o x/yyyy
+    let foundDate = '';
+    const dateRegex =
+      /\b(\d{2}\/\d{2}(\/\d{4})?|\d{2}\/\d{4}|\d{1}\/\d{4}|\d{2}\d{4})\b/;
+    for (let i = 0; i < textBlocks.length; i++) {
+      const block = textBlocks[i];
+      const matches = block.match(dateRegex);
+      if (matches) {
+        foundDate = matches[0];
+        break; // Salir del bucle al encontrar la primera fecha
+      }
+    }
+
+    // Si se encuentra un patrón como xx xxxx (sin separador), convertirlo a xx/xxxx
+    if (foundDate && /^\d{6}$/.test(foundDate)) {
+      foundDate = foundDate.slice(0, 2) + '/' + foundDate.slice(2);
+    }
+
+    // Si se encuentra un patrón como x/yyyy, agregar el cero delante del dígito del mes
+    if (foundDate && /^\d{1}\/\d{4}$/.test(foundDate)) {
+      foundDate = '0' + foundDate;
+    }
+
+    setRecognizedText(foundDate);
+    console.log('Fecha reconocida:', foundDate);
+  };
+
+  return (
+    <Container>
+      <Camera
+        captureAudio={false}
+        type={RNCamera.Constants.Type.back}
+        flashMode={RNCamera.Constants.FlashMode.off}
+        androidCameraPermissionOptions={{
+          title: 'Permission to use camera',
+          message: 'We need your permission to use your camera',
+          buttonPositive: 'Ok',
+          buttonNegative: 'Cancel',
+        }}>
+        {({camera, status}) => {
+          if (status !== 'READY') {
+            return <PendingViewComponent />;
+          }
+          return (
+            <ButtonCameraContainer>
+              <Button
+                title="Take Picture"
+                onPress={() => takePicture(camera)}
+              />
+            </ButtonCameraContainer>
+          );
+        }}
+      </Camera>
+
+      <TextRecognized>{recognizedText}</TextRecognized>
+    </Container>
+  );
+};
+
+export default HomeScreen;
--- src/views/HomeScreen/styles.ts
+++ src/views/HomeScreen/styles.ts
@@ -1,9 +1,34 @@
+import {RNCamera} from 'react-native-camera';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center;
-  justify-content: center;
+  flex-direction: column;
+  background-color: ${({theme}) => theme.colors.deepBlue};
 `;
 
-export const Text = styled.Text``;
+export const Camera = styled(RNCamera)`
+  flex: 1;
+  width: 100%;
+  margin-top: 50px;
+`;
+
+export const TextRecognized = styled.Text`
+  font-size: 18px;
+  color: ${({theme}) => theme.colors.white};
+  position: absolute;
+  margin-top: 50px;
+`;
+
+export const PendingView = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ButtonCameraContainer = styled.View`
+  position: absolute;
+  bottom: 20px;
+  width: 100%;
+  align-items: center;
+`;
